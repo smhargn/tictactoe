4b87d7f6f9bbad9736848897cd6cc031256d6444
u210709028 <smhargn@gmail.com>
2024-11-02 15:41:07 +0300

    Initial commit

---
+++ app/src/main/java/msku/ceng/madlab/week5/MainActivity.java
@@ -0,0 +1,102 @@
+package msku.ceng.madlab.week5;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String Player1 = "X";
+    static final String Player2 = "O";
+
+    boolean player1Turn = true;
+
+    byte[][] board = new byte[3][3];
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TableLayout table = findViewById(R.id.board);
+        for(int i=0; i<3; i++){
+            TableRow row =(TableRow) table.getChildAt(i);
+            //burada her bir row değeine eriştik
+            for(int j =0; j<3; j++){
+                Button button = (Button) row.getChildAt(j);
+                button.setOnClickListener(new CellListener(i,j));
+            }
+        }
+
+    }
+
+    class CellListener implements View.OnClickListener{
+        int row, col;
+
+        public CellListener(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(!isValidMove(row,col)){
+                Toast.makeText(MainActivity.this,"Cell is already occupaite",Toast.LENGTH_LONG).show();
+                return;
+            }
+            if(player1Turn){
+                ((Button)v).setText(Player1);
+                board[row][col]=1;
+            }else{
+                ((Button)v).setText(Player2);
+                board[row][col]=2;
+            }
+
+            if(gameEnd(row,col)== -1){
+                player1Turn = !player1Turn;
+
+            }else if(gameEnd(row,col)== 0){
+                Toast.makeText(MainActivity.this,"It is Draw",Toast.LENGTH_LONG).show();
+                return;
+            }else if(gameEnd(row,col)== 1){
+                Toast.makeText(MainActivity.this,"Player 1 is Win",Toast.LENGTH_LONG).show();
+                return;
+            }else{
+                Toast.makeText(MainActivity.this,"Player 2 is Win",Toast.LENGTH_LONG).show();
+                return;
+            }
+
+
+
+
+        }
+    }
+    public boolean isValidMove(int row, int col){
+        return (board[row][col] ==0 );
+    }
+    public int gameEnd(int row,int col){
+
+        int symbol = board[row][col];
+        boolean win = true;
+        for(int i =0;  i<3;i++){
+            if(board[i][col] != symbol){
+                win= false;
+                break;
+            }
+        }if(win){
+            return symbol;
+        }
+
+        return -1;
+
+    }
+}
